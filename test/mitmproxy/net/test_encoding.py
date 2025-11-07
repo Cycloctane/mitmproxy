c26a2032606b5ea16dd91fc45c34392ed37ff1f7
Rui Xi <Cycloctane@outlook.com>
2025-11-08 03:15:11 +0800

    add test

--- test/mitmproxy/net/test_encoding.py
+++ test/mitmproxy/net/test_encoding.py
@@ -112,3 +112,12 @@ def test_zstd():
 
     # Uncompressed two frames should have the size of FRAME_SIZE * 2
     assert len(encoding.decode_zstd(two_frames)) == FRAME_SIZE * 2
+
+
+@pytest.mark.parametrize("encoder", ["gzip", "deflate", "br", "zstd"])
+def test_decoders_size_limit(encoder):
+    test_content = b"a" * 1024
+    compressed = encoding.encode(test_content, encoder)
+    assert encoding.decode(compressed, encoder, size_limit=2048) == test_content
+    with pytest.raises(ValueError, match="Decompressed data exceeds size limit"):
+        encoding.decode(compressed, encoder, size_limit=512)
